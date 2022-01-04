f5945c7f6adb94fe453c8f64aba9d5bb270fb157
Mangaliso <mangaliso.twala@gmail.com>
2022-01-04 12:42:48 +0200

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newVal = event.target.value;
+
+    setInputText(newVal);
+  }
+
+  function addItem() {
+    setItems((preItems) => {
+      return [...preItems, inputText];
+    });
+
+    setInputText("");
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText} />
+        <button onClick={addItem}>
+          <span>Add</span>
+        </button>
+      </div>
+      <div>
+        <ul>
+          {items.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
